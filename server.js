b3760992645fc515bfa62114a07a49e1de77b186
Max Volovikov <maxvolovikov@gmail.com>
2018-11-09 00:50:36 -0600

    all components rendering

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8000;
+const proxy = require('http-proxy-middleware');
+const port = 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const publicDir = path.join(__dirname, 'public');
+app.enable('trust proxy');
+
+const options = {
+  target:'http://localhost:3000', 
+  changeOrigin: true, 
+  router: {
+    '/instructors':'http://localhost:3332', 
+    '/reviews':'http://localhost:8000', 
+    '/repos':'http://localhost:3333',
+    '/':'http://localhost:7777', 
+  }
+}
+
+const apiProxy = proxy(options);
+  
+app.use('/instructors', apiProxy);
+app.use('/reviews', apiProxy);
+app.use('/repos', apiProxy);
+
+app.use(express.static(publicDir));
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
