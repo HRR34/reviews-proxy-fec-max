4a915d4e716957520ac62111301e0e965003a0bf
Max Volovikov <maxvolovikov@gmail.com>
2018-11-12 02:02:22 -0600

    trying to do reviews only

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const app = express();
 const proxy = require('http-proxy-middleware');
 const port = 3000;
 
-
 const publicDir = path.join(__dirname, 'public');
 app.enable('trust proxy');
 
 const options = {
-  target:'http://localhost:3000', 
+  target:'http://localhost:3000/', 
   changeOrigin: true, 
   router: {
     '/instructors':'http://localhost:3332', 
-    '/reviews':'http://localhost:8000', 
+    '/':'http://localhost:8000', 
     '/repos':'http://localhost:3333',
     '/':'http://localhost:7777', 
   }
@@ -26,7 +25,6 @@ app.use('/reviews', apiProxy);
 app.use('/repos', apiProxy);
 
 app.use(express.static(publicDir));
-
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
